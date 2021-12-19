14d55fe6f25daa1db2dd97bb373b5c04c8a52237
Hosnihoussem <hosnihoussem188@gmail.com>
2021-12-19 14:11:34 +0100

    create-react-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import img from "./img.jpg";
+import React, { Component } from "react";
+export class App extends Component {
+  constructor() {
+    super();
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    this.state = {
+      person: {
+        fullname: "Rafik Becha",
+        profession: " 'Web Developer' ",
+        bio: " Tunis ",
+        img: img,
+        
+      },
+      x:0,
+      showProfile: false,
+    };
+  }
+  componentDidMount() {
+    document.title = "Hello";
+
+
+    setInterval(() => {
+      this.setState({x:this.state.x+1})
+    }, 1000);
+  }
+
+  btnshowProfile() {
+    this.setState({
+      showProfile: !this.state.showProfile,
+    });
+  }
+
+  render() {
+    return (
+      <div className="body">
+        {this.state.showProfile && (
+          <div>
+            <img src={this.state.person.img} alt="free"></img>
+            <h2>{this.state.person.fullname}</h2>
+            <h3>{this.state.person.profession}</h3>
+            <h4>{this.state.person.bio}</h4>
+            <h1>{this.state.x}</h1>
+          </div>
+        )}
+        <button onClick={() => this.btnshowProfile()}> {this.state.showProfile ? 'Hide Profile'   :"Show Profile"} </button>
+      </div>
+    );
+  }
 }
 
 export default App;
